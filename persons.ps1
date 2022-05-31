7ee73cf7f1e39a6eba440f8c4c77036d96bb79bd
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2022-05-31 15:26:48 +0200

    Update persons.ps1

--- persons.ps1
+++ persons.ps1
@@ -36,28 +36,6 @@ function Resolve-HTTPError {
         Write-Output $httpErrorObj
     }
 }
-
-function ConvertTo-Batches {
-    [CmdletBinding()]
-    [OutputType([System.Collections.ArrayList])]
-    param (
-        [object[]]
-        $InputArray,
-
-        [int]
-        $BatchSize
-    )
-
-    $batchArray = [System.Collections.ArrayList]::new()
-    for ($i = 0; $i -lt $InputArray.Count; $i+= $BatchSize) {
-        if (($InputArray.Count - $i) -gt $BatchSize-1  ) {
-            $null = $batchArray.add($InputArray[$i..($i + $BatchSize-1)])
-        } else {
-            $null = $batchArray.add($InputArray[$i..($InputArray.Count - 1)])
-        }
-    }
-    $batchArray
-}
 #endregion
 
 try {
@@ -72,53 +50,37 @@ try {
 
     Write-Verbose 'Retrieving student data'
     $responseStudents = Invoke-RestMethod @splatParams -Verbose:$false
+    Write-Verbose " [$($responseStudents.count)] Students found"
 
-    # The API is rate limited to a max of 50 requests per second. Therefore; we break down the students received in the
-    # $responseStudents object and separate them into batches. The size of a individual batch can be specified in the configuration
-    # but must not exceed the limit of 50.
-    #
-    # For each individual batch we will fetch the 'richStudentData' with 1 second of wait time between each batch.
-
-    Write-Verbose "Retrieving RichStudentObject in batches of [$($config.batchSize)]"
-    $batches = ConvertTo-Batches -InputArray $responseStudents.items -BatchSize $($config.batchSize)
-    $studentList = [System.Collections.Generic.List[object]]::new()
-    for ($i = 0; $i -lt $batches.count; $i++) {
-        foreach ($item in $batches[$i]) {
-            $splatGetRichStudentParams = @{
-                Uri     = "$($config.BaseUrl)/basis/student?p_studentnummer=$($item.studentnummer)"
-                Headers = $headers
-                Method  = 'GET'
-            }
-            $richStudentObj = Invoke-RestMethod @splatGetRichStudentParams -Verbose:$false
-
-            $contractList = [System.Collections.Generic.List[object]]::new()
-            foreach ($eduction in $richStudentObj.opleidingen){
-                $eduction | Add-Member -MemberType NoteProperty -Name 'ExternalId' -Value $eduction.sinh_id
-                $contractList.Add($eduction)
-            }
-
-            $student = [PSCustomObject]@{
-                ExternalId  = $item.studentnummer
-                DisplayName = "$($item.Roepnaam) $($item.achternaam)"
-                Student     = $student
-                RichStudent = $richStudentObj
-                Contracts   = $contractList
-            }
-            $studentList.Add($student)
+    Write-Verbose " Adding Contracts/Educations to Students"  
+    foreach ($item in $responseStudents.items) {
+        $splatGetRichStudentParams = @{
+            Uri     = "$($config.BaseUrl)/basis/student?p_studentnummer=$($item.studentnummer)"
+            Headers = $headers
+            Method  = 'GET'
         }
-        Start-Sleep -Seconds 1
-    }
+        $richStudentObj = Invoke-RestMethod @splatGetRichStudentParams -Verbose:$false
 
-    foreach ($person in $studentList){
-        Write-Output $person | ConvertTo-Json -Depth 60
+        $contractList = [System.Collections.Generic.List[object]]::new()
+        foreach ($eduction in $richStudentObj.opleidingen){
+            $eduction | Add-Member -MemberType NoteProperty -Name 'ExternalId' -Value $eduction.sinh_id
+            $contractList.Add($eduction)
+        }
+
+        $richStudentObj | Add-Member -MemberType NoteProperty -Name 'ExternalId' -Value $item.studentnummer
+        $richStudentObj | Add-Member -MemberType NoteProperty -Name 'DisplayName' -Value "$($item.Roepnaam) $($item.achternaam)"
+        $richStudentObj | Add-Member -MemberType NoteProperty -Name 'Contracts' -Value  $contractList
+
+        Write-Output $richStudentObj | ConvertTo-Json -Depth 10
+        Start-Sleep -MilliSeconds 10
     }
 } catch {
     $ex = $PSItem
     if ($($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or
         $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
         $errorMessage = Resolve-HTTPError -ErrorObject $ex
-        Write-Verbose "Could not retrieve Caci-Osiris employees. Error: $errorMessage"
+        Write-Verbose "Could not retrieve Caci-Osiris students. Error: $errorMessage"
     } else {
-        Write-Verbose "Could not retrieve Caci-Osiris employees. Error: $($ex.Exception.Message)"
+        Write-Verbose "Could not retrieve Caci-Osiris students. Error: $($ex.Exception.Message)"
     }
 }
